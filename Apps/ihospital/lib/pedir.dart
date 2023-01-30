b91235454bb0e3853615ce264c434b7cd6c5a9b0
Cesarquatro <caugustomendes653@gmail.com>
2023-01-30 00:17:40 -0300

    Adicionei o DropDownMenu

--- Apps/ihospital/lib/pedir.dart
+++ Apps/ihospital/lib/pedir.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'menu.dart';
 
 class pedir extends StatelessWidget {
-  String pedido = "";
-  List<String> room = <String>[
+  String? pedido;
+  final dropValue = ValueNotifier('');
+  final roomOptions = <String>[
     'room_1',
     'room_2',
     'room_3',
     'room_4',
-    'room_',
+    'room_5',
     'room_6',
     'room_7',
     'room_8',
@@ -37,19 +38,61 @@ class pedir extends StatelessWidget {
         color: Color.fromARGB(228, 48, 42, 100),
         child: Column(
           children: [
-            //DropDownMenu
             Padding(padding: EdgeInsets.all(10)),
-            TextField(
-              onChanged: (value) {
-                pedido = value;
-                print("$pedido");
+            //Adiconar DropDownMenu
+            ValueListenableBuilder(
+              valueListenable: dropValue,
+              builder: (BuildContext context, String value, _) {
+                return SizedBox(
+                  width: 300,
+                  child: DropdownButtonFormField<String>(
+                    isExpanded: true,
+                    icon: Icon(
+                      Icons.meeting_room,
+                      color: Colors.black,
+                    ),
+                    hint: Text(
+                      "Selecione seu quarto.",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      label: Text('Quarto'),
+                      border: OutlineInputBorder(),
+                    ),
+                    value: (value.isEmpty) ? null : value,
+                    onChanged: (escolha) =>
+                        dropValue.value = escolha.toString(),
+                    items: roomOptions
+                        .map(
+                          (opcao) => DropdownMenuItem(
+                            value: opcao,
+                            child: Text(opcao),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                );
               },
-              keyboardType: TextInputType.name,
-              decoration: InputDecoration(
-                labelText: "Pedido",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+              child: TextField(
+                onChanged: (value) {
+                  pedido = value;
+                  print("$pedido");
+                },
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "Pedido",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
             Padding(padding: EdgeInsets.all(5)),
